1cdd7d342a5a8a85e9eeeb1baa81fe9a47d3fb7f
buschinvadim <bushin.1999@mail.ru>
2022-02-21 22:15:29 +0300

    edit gitignore

---
+++ class.py
@@ -0,0 +1,23 @@
+# class SomeClass:
+#
+#     def __init__(self, a):
+#         self.a = a
+#
+#     def instanse_func(self):
+#         ...
+#
+#     @classmethod
+#     def class_func(self):
+#         ...
+#
+#     @staticmethod
+#     def static_func(self):
+#
+#
+#
+# obj1 = SomeClass(100)
+# obj2 = SomeClass(100)
+
+
+
+
